135c882ff24ffc8a9a532b2816591f3de97adc0a
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-11 07:35:44 -0600

    splendid generator and quest stuff

--- kubejs/server_scripts/firstLogin.js
+++ kubejs/server_scripts/firstLogin.js
@@ -1,8 +1,7 @@
 PlayerEvents.loggedIn(e => {
     if (!e.hasGameStage('started')) {
-        e.player.give('ftbquests:book')
-        e.player.give('waystones:waystone')
         e.player.give('splendid_slimes:slime_vac')
+        e.player.give('ftbquests:book')
         e.player.give('numismatic_utils:bank_meter')
 
         e.addGameStage('started')
